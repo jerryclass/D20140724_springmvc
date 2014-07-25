87815f06a35431f586d500b7bdbd9c4c4a8dc4a3
Jerry Wu <jerryclass@gmail.com>
2014-07-25 14:45:22 +0800

    定文想多留幾年
    定文想多留幾年

---
+++ src/tw/edu/kuas/mis/m1101405110/SecondController.java
@@ -0,0 +1,16 @@
+package tw.edu.kuas.mis.m1101405110;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SecondController {
+	
+	
+	@RequestMapping("/say")
+	public String sayHello(Model model)
+	{
+		return "helloWorld";
+	}
+}
