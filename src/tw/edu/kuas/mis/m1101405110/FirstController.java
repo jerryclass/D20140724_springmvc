00b94c307492fc67b4e38cc0b7fb4706a9c79a1e
Jerry Wu <jerryclass@gmail.com>
2014-07-25 17:13:32 +0800

    test
    test

--- src/tw/edu/kuas/mis/m1101405110/FirstController.java
+++ src/tw/edu/kuas/mis/m1101405110/FirstController.java
@@ -1,22 +1,32 @@
 package tw.edu.kuas.mis.m1101405110;
 
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 @RequestMapping("/first/{uid}")
 public class FirstController {
     
-	@RequestMapping("/test01")
-	public String test01(Model model,@RequestParam("userName") String name)
+	@RequestMapping(value="/test01",method=RequestMethod.POST)
+	@ResponseBody
+	public String test01(Model model,
+			@RequestParam(value="userName"  , required=false , defaultValue="no user name") String name , 
+			@RequestBody String temp
+			)
 	{
-		model.addAttribute("name", name);
-		return "test1";
+	
+		return temp;
+		
+		
 	}
 	
 	
