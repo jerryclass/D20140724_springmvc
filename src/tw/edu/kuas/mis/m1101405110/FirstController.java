9d6211a3c55845f6b3c6090934cf851ab33546a1
Jerry Wu <jerryclass@gmail.com>
2014-07-25 15:28:53 +0800

    tets
    etst

--- src/tw/edu/kuas/mis/m1101405110/FirstController.java
+++ src/tw/edu/kuas/mis/m1101405110/FirstController.java
@@ -5,12 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 @RequestMapping("/first/{uid}")
 public class FirstController {
     
+	@RequestMapping("/test01")
+	public String test01(Model model,@RequestParam("userName") String name)
+	{
+		model.addAttribute("name", name);
+		return "test1";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@RequestMapping(value="/User/{school}/{userName}",method=RequestMethod.POST)
     public String helloWorld(Model model,
     		@PathVariable("userName") String name,
